f086c185e7d448ab42c3a5a66399da8e2306e5b5
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2024-06-01 01:08:29 +0600

    Update Addition,Sub,Multi,Div.java

--- Addition,Sub,Multi,Div.java
+++ Addition,Sub,Multi,Div.java
@@ -1,1 +1,101 @@
-import 
+// Create Simple Calculator using GUI.
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        // Create the frame
+        JFrame frame = new JFrame("Simple Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLayout(new GridLayout(5, 2));
+
+        // Create the components
+        JLabel label1 = new JLabel("Number 1:");
+        JLabel label2 = new JLabel("Number 2:");
+        JLabel resultLabel = new JLabel("Result:");
+        JLabel result = new JLabel();
+        frame.add(label1);
+        frame.add(label2);
+        frame.add(resultLabel);
+        frame.add(result);
+
+        JTextField textField1 = new JTextField();
+        JTextField textField2 = new JTextField();
+        frame.add(textField1);
+        frame.add(textField2);
+
+        JButton addButton = new JButton("Add");
+        JButton subtractButton = new JButton("Subtract");
+        JButton multiplyButton = new JButton("Multiply");
+        JButton divideButton = new JButton("Divide");
+        frame.add(addButton);
+        frame.add(subtractButton);
+        frame.add(multiplyButton);
+        frame.add(divideButton);
+       
+
+        // Define action listeners for the buttons
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double num1 = Double.parseDouble(textField1.getText());
+                    double num2 = Double.parseDouble(textField2.getText());
+                    double sum = num1 + num2;
+                    result.setText(String.valueOf(sum));
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(frame, "Please enter valid numbers", "Input Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        subtractButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double num1 = Double.parseDouble(textField1.getText());
+                    double num2 = Double.parseDouble(textField2.getText());
+                    double difference = num1 - num2;
+                    result.setText(String.valueOf(difference));
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(frame, "Please enter valid numbers", "Input Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        multiplyButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double num1 = Double.parseDouble(textField1.getText());
+                    double num2 = Double.parseDouble(textField2.getText());
+                    double product = num1 * num2;
+                    result.setText(String.valueOf(product));
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(frame, "Please enter valid numbers", "Input Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        divideButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    double num1 = Double.parseDouble(textField1.getText());
+                    double num2 = Double.parseDouble(textField2.getText());
+                    if (num2 != 0) {
+                        double quotient = num1 / num2;
+                        result.setText(String.valueOf(quotient));
+                    } else {
+                        JOptionPane.showMessageDialog(frame, "Cannot divide by zero", "Math Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(frame, "Please enter valid numbers", "Input Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        // Set the frame visibility to true
+        frame.setVisible(true);
+    }
+}
